a7e60cef86864fb26dde9fd1651fb7b2d8157ceb
stefaanbeernaert <beernaertstefaan@gmail.com>
2022-07-23 04:01:12 +0200

    votes

--- resources/views/livewire/idea-index.blade.php
+++ resources/views/livewire/idea-index.blade.php
@@ -18,11 +18,13 @@
         <div class="mt-8">
             @if($hasVoted)
                 <button
+                    wire:click.prevent="vote"
                     class="w-20 bg-blue border border-blue hover:bg-blue-hover text-white font-bold text-xxs uppercase rounded-xl transition duration-150 ease-in px-4 py-3">
                     Voted
                 </button>
             @else
                 <button
+                    wire:click.prevent="vote"
                     class="w-20 bg-gray-200 border border-gray-200 hover:border-gray-400 font-bold text-xxs uppercase rounded-xl transition duration-150 ease-in px-4 py-3">
                     Vote
                 </button>
@@ -95,12 +97,14 @@
                     </div>
                     @if($hasVoted)
                         <button
+                            wire:click.prevent="vote"
                             class="w-20 bg-blue border border-blue text-white font-bold text-xxs uppercase rounded-xl hover:bg-blue-hover transition duration-150 ease-in px-4 py-3 -mx-5"
                         >
                             Voted
                         </button>
                     @else
                         <button
+                            wire:click.prevent="vote"
                             class="w-20 bg-gray-200 border border-gray-200 font-bold text-xxs uppercase rounded-xl hover:border-gray-400 transition duration-150 ease-in px-4 py-3 -mx-5"
                         >
                             Vote
