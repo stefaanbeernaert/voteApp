cfa6a24b26a74ae5351e363aceb20ffbac0a103e
stefaanbeernaert <beernaertstefaan@gmail.com>
2022-07-28 18:36:38 +0200

    27

--- app/Http/Livewire/StatusFilters.php
+++ app/Http/Livewire/StatusFilters.php
@@ -10,33 +10,29 @@ use Livewire\Component;
 
 class StatusFilters extends Component
 {
-    public $status = 'All';
+    public $status;
     public $statusCount;
-
-    protected $queryString = [
-        'status',
-    ];
 
     public function mount(){
 
         $this->statusCount = Status::getCount();
+        $this->status = request()->status ?? 'All';
+
         if (Route::currentRouteName() === 'idea.show'){
             $this->status = null;
-            $this->queryString = [];
         }
     }
 
     public function setStatus($newStatus){
         $this->status = $newStatus;
+        $this->emit('queryStringUpdatedStatus', $this->status);
 
-        /*if ($this->getPreviousRouteName() === 'idea.show'){*/
+        if ($this->getPreviousRouteName() === 'idea.show'){
             return redirect()->route('idea.index', [
                 'status' => $this->status,
 
-
-
         ]);
-      /*  }*/
+       }
     }
     public function getPreviousRouteName(){
         return app('router')->getRoutes()->match(app('request')->create(url()->previous()))->getName();
